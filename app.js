d59545b068e60e183061a4ec147a92dbda7e9313
Rafael Bonaldo <dev.bonaldo@gmail.com>
2022-12-20 14:05:46 -0300

    bug fix event prevent

--- app.js
+++ app.js
@@ -7,18 +7,29 @@ const screen_2 = document.getElementsByClassName("submit__screen");
 
 let collectionRatingValues = [];
 
+let selectedValue = false;
+let choice = "";
+
 for (var i = 0; i < inputsRating.length; i++) {
   collectionRatingValues.push(inputsRating[i]);
 }
 
 collectionRatingValues.forEach((action) => {
   action.addEventListener("click", () => {
-    const choice = action.getAttribute("value");
+    choice = action.getAttribute("value");
+    selectedValue = true;
+  });
+});
+
+btnSubmit.addEventListener("click", (event) => {
+  if (selectedValue == true) {
     screen_1[0].classList.add("hidden");
     screen_2[0].classList.remove("hidden");
 
     submitNote[0].innerHTML = `
-    <span>You selected ${choice} out of ${collectionRatingValues.length}</span>
-    `;
-  });
+  <span>You selected ${choice} out of ${collectionRatingValues.length}</span>
+  `;
+  }
+
+  event.preventDefault();
 });
